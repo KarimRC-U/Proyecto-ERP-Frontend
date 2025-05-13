c6d300e931b0ed6b5125b03fadb6060bce080b8e
Akari <ag.almanzaacosta@ugto.mx>
2025-05-13 09:00:16 -0600

    Todas las carpetas creadas

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <login-user />
+  </v-container>
+</template>
+
+<script>
+// import loginUser from '~/components/auth/loginUser.vue'
+import loginUser from '~/components/patients/patientsAll.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    loginUser
+  }
+}
+</script>
